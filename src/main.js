6090752b57cc77efe05eed82b33d3471484e0b59
mmeerrkkaa <55891034+mmeerrkkaa@users.noreply.github.com>
2024-09-10 22:24:21 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const { createBot } = require('blockmind');
 const CustomRepository = require("./database/repositories/customRepository");
+const {handleAuthMessage, handleWorldType} = require("./utils/joinStaffHandlers");
 
 const botOptions = {
     host: 'mc.masedworld.net',
@@ -22,8 +23,27 @@ createBot(botOptions).then(async (bot) => {
     const customRepository = new CustomRepository();
     await customRepository.initialize();
 
+    let wasInHub = false;
+
 
     const newItem = await customRepository.create({ name: 'testItem', value: 10 });
     const item = await customRepository.findByName('testItem');
     console.log(item);
+
+    bot.on('spawn', async () => {
+        console.log('Bot spawned.');
+        const worldtype = await handleWorldType(bot);
+        if (worldtype === 'Hub') {
+            global.wasInHub = true;
+            await bot.sendMessage('local', '/surv' + bot.MC_SERVER);
+        } else if (worldtype === 'Survival') {
+            // Бот авторизовался.
+        }
+    });
+
+    bot.on('message', async (jsonMsg) => {
+        const message = jsonMsg.toString();
+        console.log(message);
+        if (!wasInHub) await handleAuthMessage(bot, message);
+    });
 });
